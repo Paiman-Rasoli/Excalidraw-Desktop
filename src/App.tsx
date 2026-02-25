4907d467ccf2f26f0fcb88f9aec05f3d6f1f1544
Paiman-Rasoli <paimanrasoli789@gmail.com>
2026-02-26 03:56:11 +0430

    feat: presist library data to local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
 import "@excalidraw/excalidraw/index.css";
-import { lazy, Suspense, useState } from "react";
+import { lazy, Suspense, useEffect, useState } from "react";
 import { Footer } from "@excalidraw/excalidraw";
+import { invoke } from "@tauri-apps/api/core";
 import { LibraryModal } from "./components/library-modal";
+import { ExcalidrawImperativeAPI, LibraryItems } from "@excalidraw/excalidraw/types";
 
 const Excalidraw = lazy(() =>
   import("@excalidraw/excalidraw").then((module) => ({
@@ -12,7 +14,41 @@ const Excalidraw = lazy(() =>
 
 function App() {
   const [isLibraryModalOpen, setIsLibraryModalOpen] = useState(false);
+  const [excalidrawAPI, setExcalidrawAPI] = useState<ExcalidrawImperativeAPI | null>(null);
   const libraryUrl = "https://libraries.excalidraw.com/";
+
+  useEffect(() => {
+    const loadStoredItems = async () => {
+      try {
+        const libraryItems = await invoke<LibraryItems>("load_library_items");
+
+        if (Array.isArray(libraryItems) && excalidrawAPI) {
+
+          if (libraryItems.length > 0) {
+            excalidrawAPI.updateLibrary({
+              libraryItems,
+              openLibraryMenu: true,
+            });
+          }
+        }
+
+      } catch (error) {
+        console.error("Failed to load stored library items", error);
+      }
+    };
+
+    if (excalidrawAPI) {
+      void loadStoredItems();
+    }
+  }, [excalidrawAPI]);
+
+  const handleLibraryChange = async (items: LibraryItems) => {
+    try {
+      await invoke("save_library_items", { items: [...items] });
+    } catch (error) {
+      console.error("Failed to save library items", error);
+    }
+  };
 
   return (
     <main className="container">
@@ -30,7 +66,12 @@ function App() {
           </div>
         }
       >
-        <Excalidraw>
+        <Excalidraw
+          excalidrawAPI={(api) => setExcalidrawAPI(api)}
+          onLibraryChange={(items) => {
+            handleLibraryChange(items);
+          }}
+        >
           <Footer>
             <button
               className="custom-footer"
